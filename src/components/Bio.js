93bbbae8e9ab5e532bf3fc48185c25c9202a55a9
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-05-23 22:47:52 +0800

    Deploy

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,7 +13,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Dan Abramov`}
+          alt={`Ray Toh`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
